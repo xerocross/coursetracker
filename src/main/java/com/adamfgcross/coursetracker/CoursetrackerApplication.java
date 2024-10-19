e75e04f43648f5713690e281003ddf2f79697776
Adam <adamfgcross@gmail.com>
2024-10-19 12:28:08 -0400

    initial commit

---
+++ src/main/java/com/adamfgcross/coursetracker/CoursetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.coursetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamfgcross/coursetracker/CoursetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.coursetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
